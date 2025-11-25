9bfa45d980da868c8d99865ed5a9f12a3be83628
giannivitale <giavitale78@gmail.com>
2025-11-25 16:58:05 +0100

    refactor: reorganize posts route and implement article data structure

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,53 @@
 const express = require('express')
 const router = express.Router();
 
+
+const articles = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "Buonissimo",
+        immagine: "/img/ciambellone.jpeg",
+        tags: ["dolce", "colazione", "semplice"],
+    },
+    {
+        titolo: "Cracker di Barbabietola",
+        contenuto: "Ma cosa sto guardando",
+        immagine: "/img/cracker_barbabietola.jpeg",
+        tags: ["snack", "salato", "veloce"],
+    },
+    {
+        titolo: "Pane Fritto Dolce",
+        contenuto: "Ci ho ripensato, da NON provare",
+        immagine: "/img/pane_fritto_dolce.jpeg",
+        tags: ["dolce", "merenda", "tradizione"],
+    },
+    {
+        titolo: "Pasta alla Barbabietola",
+        contenuto:
+            "Sembra quel tipico piatto da ristorante a 5 stelle che lo provi e sa di legno",
+        immagine: "/img/pasta_barbabietola.jpeg",
+        tags: ["pasta", "vegetariano", "veloce"],
+    },
+    {
+        titolo: "Torta Paesana",
+        contenuto: "Immagini IA tipo:",
+        immagine: "/img/torta_paesana.jpeg",
+        tags: ["dolce", "rustico", "tradizione"],
+    },
+];
+
+
+
 //Index
 router.get('/', (req, res) => {
-    res.send('Hello Boolean Blog!')
+    let list = ""
+    
+
+
+    return res.send('Hello Boolean Blog!')
 })
+   
+   
 
 //show
 router.get('/:id', (req, res) => {
@@ -14,7 +57,7 @@ router.get('/:id', (req, res) => {
 
 //store
 router.post('/', (req, res) => {
-    res.send('Creazione nuovo articolo' + req.params.id);
+    res.send('Creazione nuovo articolo');
 });
 
 
@@ -25,12 +68,12 @@ router.put('/:id', (req, res) => {
 
 //modify
 router.patch('/:id', (req, res) => {
-    res.send('Modifica parziale degli articoli' + res.params.id);
+    res.send('Modifica parziale degli articoli' + req.params.id);
 });
 
 //destroy
 router.delete('/:id', (req, res) => {
-    res.send('Eliminazione degli articoli' + res.params.id);
+    res.send('Eliminazione degli articoli' + req.params.id);
 })
 
 
--- server.js
+++ server.js
@@ -3,80 +3,15 @@ const app = express()
 const PORT = 3000
 const postRouter = require('./routes/posts')
 
-app.listen(PORT, () => {
-    console.log(`Example my blog is listening on http://localhost:${PORT} `);
-    
+
+app.get('/', (req,res) =>{
+    return res.send('Welcome to my blog')
 })
 
-const articles = [
-    {
-        titolo: "Ciambellone",
-        contenuto: "Buonissimo",
-        immagine: "/img/ciambellone.jpeg",
-        tags: ["dolce", "colazione", "semplice"],
-    },
-    {
-        titolo: "Cracker di Barbabietola",
-        contenuto: "Ma cosa sto guardando",
-        immagine: "/img/cracker_barbabietola.jpeg",
-        tags: ["snack", "salato", "veloce"],
-    },
-    {
-        titolo: "Pane Fritto Dolce",
-        contenuto: "Ci ho ripensato, da NON provare",
-        immagine: "/img/pane_fritto_dolce.jpeg",
-        tags: ["dolce", "merenda", "tradizione"],
-    },
-    {
-        titolo: "Pasta alla Barbabietola",
-        contenuto:
-            "Sembra quel tipico piatto da ristorante a 5 stelle che lo provi e sa di legno",
-        immagine: "/img/pasta_barbabietola.jpeg",
-        tags: ["pasta", "vegetariano", "veloce"],
-    },
-    {
-        titolo: "Torta Paesana",
-        contenuto: "Immagini IA tipo:",
-        immagine: "/img/torta_paesana.jpeg",
-        tags: ["dolce", "rustico", "tradizione"],
-    },
-];
-
-// //root
-// app.get('/', (req,res) => {
-//     res.send('Welcome to my Blog')
-// })
-
-
-// // Index
-// app.get('/articles' , (req, res) => {
-//     res.send('Hello Boolean Blog!')
-//  })
-
-// //show
-// app.get('api/articles/:id', (req, res) => {
-//   res.send('Dettagli del Blod' + req.params.id);
-// })
-
-// //store
-// app.post('api/articles', (req, res) => {
-//      res.send('Creazione nuovo articolo' + req.params.id);
-// });
-
-
-// //update
-// app.put('api/articles/:id', (req, res) => {
-//   res.send('Modifica integrale degli articoli' + req.params.id);
-// });
-
-//  //modify
-// app.patch('api/articles/:id', (req,res) => {
-//     res.send('Modifica parziale degli articoli' + res.params.id);
-//  });
-
-//  //destroy
-//  app.delete('api/articles/:id', (req, res) => {
-//     res.send('Eliminazione degli articoli' + res.params.id);
-// })
-
 app.use('/posts', postRouter)
+
+
+app.listen(PORT, () => {
+    console.log(`Example my blog is listening on http://localhost:${PORT} `);
+
+})
